89ce83c0dd8bb41c076f2f9053b4379ddc6abec7
anjitha2002 <anjithaprasadcse@gmail.com>
2025-12-23 00:41:39 +0400

    access right fields

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': "Institution Management System",
     'version': '19.0.1.0.0',
-    'category': 'Students, Teacher,Course,Batch',
+    'category': 'Education',
     'sequence':6,
     'summary':'Get details of institutions',
     'description': """
@@ -9,12 +9,17 @@
        Manages :
 
 """,
-    'depends':[],
+    'depends':['base',],
 'data':[
-    'data/student_sequence.xml',
+
+    'security/institution_security_group.xml',
+    'security/institution_rule.xml',
     'security/ir.model.access.csv',
+
+    'data/student_sequence.xml',
+
     'views/student_views.xml',
-'views/teacher_views.xml',
+    'views/teacher_views.xml',
     'views/course_views.xml',
     'views/category_views.xml',
     'views/batch_views.xml',
